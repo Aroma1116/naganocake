76dda8ef1585840e43ed117114e787847e8ae959
Aroma1116 <aroma1116@outlook.jp>
2022-05-07 08:44:27 +0000

    認証機能終了

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,12 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_kana, presence: true
+  validates :last_kana, presence: true
+  validates :postal_code, presence: true
+  validates :address, presence: true
+  validates :tel_number, presence: true
 end
