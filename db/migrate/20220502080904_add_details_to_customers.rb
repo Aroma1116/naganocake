f7fbbb534e114c3b2ed57c5531ac9c228cd66c72
Aroma1116 <aroma1116@outlook.jp>
2022-05-08 05:37:08 +0000

    アカウント管理機能実装開始

--- db/migrate/20220502080904_add_details_to_customers.rb
+++ db/migrate/20220502080904_add_details_to_customers.rb
@@ -7,6 +7,6 @@ class AddDetailsToCustomers < ActiveRecord::Migration[6.1]
     add_column :customers, :postal_code, :string
     add_column :customers, :address, :string
     add_column :customers, :tel_number, :string
-    add_column :customers, :is_unsubscribed, :boolean
+    add_column :customers, :is_unsubscribed, :boolean, default: false
   end
 end
