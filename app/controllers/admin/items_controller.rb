2c17de0d10531c7ce1082514ded1cb3fdccaed75
Aroma1116 <aroma1116@outlook.jp>
2022-05-14 04:38:14 +0000

    一旦完了

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -10,8 +10,12 @@ class Admin::ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @item.save
-    redirect_to admin_item_path(@item)
+   if @item.save
+     redirect_to admin_item_path(@item)
+   else
+     @genres = Genre.all
+     render :new
+   end
   end
 
   def show
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,11 @@ class Item < ApplicationRecord
   belongs_to :genre
   has_one_attached :item_image
 
+  validates :name, presence:true
+  validates :introduction, presence:true
+  validates :price, presence:true
+
+
   def get_item_image(width, height)
     unless item_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
