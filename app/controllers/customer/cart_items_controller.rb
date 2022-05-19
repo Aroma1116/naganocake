1afd8c7b909b9c2b6dbbaeff4fa87984a2bd0093
Aroma1116 <aroma1116@outlook.jp>
2022-05-19 08:54:10 +0000

    機能実装完了、後はレイアウトなど

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -1,12 +1,14 @@
 class Customer::CartItemsController < ApplicationController
+  before_action :correct_customer, only: [:index, :create, :destroy, :destroy_all]
+
   def index
     @cart_items = CartItem.all
+    @total = @cart_items.inject(0) { |sum, cart_item| sum +  cart_item.subtotal }
   end
 
   def create
     @cart_item = CartItem.new(cart_item_params)
     cart_item = CartItem.find_by(customer_id: current_customer.id, item_id: cart_item_params[:item_id])
-    byebug
     if cart_item
       new_quantity = cart_item.amount + @cart_item.amount
       cart_item.update_attribute(:amount, new_quantity)
@@ -17,6 +19,12 @@ class Customer::CartItemsController < ApplicationController
       @cart_item.save
       redirect_to customer_cart_items_path
     end
+  end
+
+  def update
+    @cartitem = CartItem.find(params[:id])
+    @cartitem.update(amount: params[:amount].to_i)
+    redirect_to customer_cart_items_path
   end
 
   def destroy
@@ -35,4 +43,11 @@ class Customer::CartItemsController < ApplicationController
   def cart_item_params
     params.require(:cart_item).permit(:item_id, :amount)
   end
+
+  def correct_customer
+    unless customer_signed_in?
+      redirect_to customer_items_path
+    end
+  end
+
 end
